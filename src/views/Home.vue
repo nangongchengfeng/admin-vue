047947c3d32c80435e5d2699229bb71b17cde2b3
南宫乘风 <1794748404@qq.com>
2024-04-29 17:17:40 +0800

    主页页面布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,41 @@
 <template>
-    <h2>主页</h2>
+    <el-container class="home-container">
+        <el-aside width="200px">aside</el-aside>
+        <el-container>
+            <el-header>header</el-header>
+            <el-main>main</el-main>
+        </el-container>
+
+    </el-container>
 </template>
 <script>
+export default {
+    name: "Home",
+    data() {
+        return {};
+    },
+    created() { },
+    methods: {}
+};
 </script>
-<style>
+<style lang="less" scoped>
+.home-container {
+    height: 100%;
+
+    .el-aside {
+        background-color: #d3dce6;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
+    }
+
+    .el-header {
+        background-color: #435466;
+    }
+
+    .el-main {
+        background-color: #204b75;
+    }
+
+}
 </style>
